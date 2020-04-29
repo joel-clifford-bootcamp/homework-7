8aeb1e9d3a1b0b25cd32c197df6d1b2ad2b65910
Joel Clifford <joel.clifford.bootcamp@gmail.com>
2020-04-29 00:15:21 -0400

    added validation to github username - verify that user exists and has repos

--- index.js
+++ index.js
@@ -6,6 +6,45 @@ let gitHubUserData;
 let gitHubRepos;
 let selectedRepo;
 
+// Verify that a given GitHub exists and has at least one repo
+function verifyGitHubAccount(username) {
+
+    return new Promise((resolve,reject) => {
+
+        url = `https://api.github.com/users/${username}`;
+
+        // User GitHubUser Data
+        axios
+        .get(url)
+        .then(resp => {
+
+            gitHubUserData = resp.data;
+
+            return resp.data.repos_url;
+        })
+        .catch(err => {
+            reject(new Error(`GitHub username ${username} does not exist`));
+        })
+        .then(reposUrl => {
+        
+            axios
+            .get(reposUrl)
+            .then(resp => {
+
+                const reposData = resp.data;
+
+                if(reposData.length === 0){
+                    reject(new Error(`GitHub user ${username} has no repos`));
+                }
+
+                gitHubRepos = reposData;
+
+                resolve(true);
+            })
+        })
+    })
+}
+
 const questions = [
     {
         name: "title",
@@ -47,73 +86,47 @@ function init() {
     inquirer.prompt({
             name: "username",
             message: "What is your GitHub username?",
-            default: "joel-clifford-bootcamp"
-        })
-        // Retrieve repo corresponding to username
+            default: "joel-clifford-bootcamp",
+            validate: verifyGitHubAccount
+    })
+    // Retrieve repo corresponding to username
+    .then(resp => {
+
+        const repoPrompt = [{type: "list",
+            name: "repoName",
+            message: "Select the project repo:",
+            choices: gitHubRepos.map(repo => repo.name)}];
+
+        // Prompt user to select which repo the README is for
+        inquirer
+        .prompt(repoPrompt)
         .then(resp => {
 
-            const username = resp.username;
+            selectedRepo = gitHubRepos.find(repo => repo.name == resp.repoName);
 
-            url = `https://api.github.com/users/${username}`;
+            // Set repo name and description as defaults for Title and Description 
+            // question
+            questions[0].default = selectedRepo.name;
+            questions[1].default = selectedRepo.description;
 
-            axios.get(url).then(resp => {
-            
-            gitHubUserData = resp.data;
+            // Get contributors and tags from repo
+            axios
+            .all([
+                axios.get(selectedRepo.contributors_url),
+                axios.get(selectedRepo.tags_url)
+            ])
+            .then(respArr => {
+                // Set repo contributors as default for Contributors question
+                questions[5].default = respArr[0].data.map(contributor => contributor.login).join(',');
 
-            return resp.data;
-
-        })
-        .then(resp => {
-
-            reposUrl = resp.repos_url;
-
-            // Get list of user's repos
-            axios.get(reposUrl).then(resp => {
-
-            gitHubRepos = resp.data;
-
-            const repoPrompt = [{type: "list",
-                name: "repoName",
-                message: "Select the project repo:",
-                choices: resp.data.map(repo => repo.name)}];
-
-            // Prompt user to select which repo the README is for
-            inquirer.prompt(repoPrompt).then(resp => {
-
-                selectedRepo = gitHubRepos.find(repo => repo.name == resp.repoName);
-
-                // Set repo name and description as defaults for Title and Description 
-                // question
-                questions[0].default = selectedRepo.name;
-                questions[1].default = selectedRepo.description;
-
-                // Get contributors and tags from repo
-                axios.all([
-                    axios.get(selectedRepo.contributors_url),
-                    axios.get(selectedRepo.tags_url)
-
-                ]).then(respArr => {
-
-                    // Set repo contributors as default for Contributors question
-                    questions[5].default = respArr[0].data.map(contributor => contributor.login).join(',');
-
-                    inquirer.prompt(questions).then(resp => {
-                    
-                        
+                inquirer
+                .prompt(questions)
+                .then(resp => {
+                
                 })
             })
         })
     })
-})
-        .catch(error => {
-
-            console.log(`Username '${answer.username}' not found!`);
-
-        });
-    });
-
 }
 
 init();
-
-
